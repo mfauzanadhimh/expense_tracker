8a2dd1aec34c4340ade89b09e0855fb582da46be
mfauzanadhimh <muhammadfauzanadhimhusnuddin@mail.ugm.ac.id>
2026-09-16 09:34:48 +0700

    sudah memperbaiki main agar sesuai dengan update sebelumnya

--- expense-api/src/main/kotlin/Main.kt
+++ expense-api/src/main/kotlin/Main.kt
@@ -17,6 +17,12 @@ val service = ExpenseService(repo)
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val repository = ExpenseRepository()
+
+    val service = ExpenseService(repository)
+
+    val controller = ExpenseController(service)
+
     embeddedServer(Netty, port = 8080) {
         install(ContentNegotiation) {
             json()
@@ -24,46 +30,24 @@ fun main() {
 
         //tes upload lewat intlij
         routing{
-            get("/"){
-                call.respondText("Hello, Expense API!")
+            get("/expenses/{id}"){
+                controller.getExpense(call)
             }
+
             post("/expenses"){
-                val expense = call.receive<Pengeluaran>()
-                    ?: return@post call.respond(HttpStatusCode.BadRequest, "Input tidak valid")
-                repo.tambah(expense)
-                call.respond(expense)
+                controller.postExpense(call)
             }
-                val id = call.parameters["id"]?.toIntOrNull()
-                    ?: return@get call.respond(HttpStatusCode.BadRequest, "ID tidak valid")
 
-                val expense = repo.tampilkan(id)
-                    ?: return@get call.respond(HttpStatusCode.NotFound, "Pengeluaran tidak ditemukan")
-
-                call.respond(expense)
-            }
             put("/expenses/{id}"){
-
-
-                val expenseBaru = call.receive<Pengeluaran>()
-
-                val expenses = repo.edit(id, expenseBaru)
-                    ?: return@put call.respond(HttpStatusCode.NotFound, "Pengeluaran tidak ditemukan")
-
-                call.respond(expenses)
+                controller.putExpense(call)
             }
+
             delete("/expenses/{id}"){
-                val id = call.parameters["id"]?.toIntOrNull()
-                    ?: return@delete call.respond(HttpStatusCode.BadRequest, "ID tidak valid")
-
-                val expenses = repo.hapus(id)
-                    ?: return@delete call.respond(HttpStatusCode.NotFound, "Tidak ada pengeluaran")
-                call.respond(expenses)
+                controller.deleteExpense(call)
             }
-            get("/expenses/total"){
-                val total_uang = repo.total()
-                    ?: return@get call.respond(HttpStatusCode.NotFound, "Masih Kosong")
 
-                call.respond(total_uang)
+            get("/expenses/total"){
+                controller.getTotalExpense(call)
             }
         }
     }.start(wait = true)
